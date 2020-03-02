c33e3ffe585c48e393b2a2c2df4085da46b04f77
Rocco-Estrada <roccoestrada@gmail.com>
2020-03-02 15:06:19 -0800

    Enemies instantiation and score implemented

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,44 +1,62 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     public GameObject A;
-
     public GameObject B;
-
     public GameObject C;
-
     public GameObject D;
 
     public Transform parentTransform;
 
     private int numEnemy = 11;
+    private int row = 4;
+
+    public ScoreManager score;
+
 
     // Start is called before the first frame update
     void Start()
     {
-
-        enemyLayout();
+        score = gameObject.AddComponent<ScoreManager>();
+        InstantiateEnemyRow();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
-    void enemyLayout()
+    void InstantiateEnemyRow()
     {
         GameObject enemyInst;
-
-        if (this.gameObject.name == "Enemy C")
+        GameObject enemyType;
+        for(int j = 0; j < row; j++)
         {
-            enemyInst = GameObject.Instantiate(C, parentTransform);
-            enemyInst.transform.localPosition = new Vector2(parentTransform.position.x, 0);
-        }
+            for (int i = 0; i < numEnemy; i++)
+            {
+                if(j == 0) { enemyType = C; }
+                else if(j > 0 && j < 3) { enemyType = B; }
+                else if(j > 2 && j < 5) { enemyType = A; }
+                else { enemyType = D; }
+                enemyInst = GameObject.Instantiate(enemyType, parentTransform);
+                enemyInst.transform.localPosition = new Vector2(i + 1, -j);
+            }
+        }   
     }
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Bullet"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+            score.Update(name);
+           
+        }
+    }
 
 }
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class ScoreManager : MonoBehaviour
+{
+
+    [SerializeField] private Text scoreText;
+    [SerializeField] private Text hiScoreText;
+
+
+    private static int score = 0;
+    private static int hiScore = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        hiScoreText = GetComponent<Text>();
+        scoreText.text = score.ToString("D4");
+        hiScoreText.text = hiScore.ToString("D4");
+
+    }
+
+    private void Update()
+    {
+        scoreText.text = score.ToString("D4");
+        hiScore = score;
+        hiScoreText.text = hiScore.ToString("D4");
+        Restart();
+    }
+
+    public void Update(string name)
+    {
+        if (name == "Enemy A(Clone)") { score += 10; }
+        else if (name == "Enemy B(Clone)") { score += 20; }
+        else if (name == "Enemy C(Clone)") { score += 30; }
+        else if (name == "Enemy D") { score += 40; }
+    }
+
+    void Restart()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            hiScore = 0;
+            score = 0;
+            SceneManager.LoadScene("Space Invaders");
+        }
+    }
+}
